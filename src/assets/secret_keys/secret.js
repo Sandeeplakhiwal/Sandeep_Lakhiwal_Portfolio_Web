06ba34305e81b8642daef10a97c9b9282fb2f3a1
Sandeep_Lakhiwal <sandeeplakhiwal98@gmail.com>
2023-06-02 21:05:23 +0530

    Delete - empty unused folder

---
+++ src/assets/secret_keys/secret.js
@@ -0,0 +1,8 @@
+/* EmailJS Keys */
+const firstKey = "service_skjlczm";
+const secondKey = "template_ea1y5tu";
+const thirdKey = "Zw4M8r2PCVYIAJCJj";
+const toEmail = "sandeeplakhiwal98@gmail.com";
+const toName = "Sandeep Lakhiwal";
+
+export { firstKey, secondKey, thirdKey, toEmail, toName };
